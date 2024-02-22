9f5b49724db1682e4bda360ae8db7d8fd1b80b3f
swtrjsuth <43635469+swtrjsuth@users.noreply.github.com>
2024-02-22 13:01:54 +1100

    1708567313

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 import urllib.request
 import json
+import os
 
-payload = {'test':'123'}
+ENV_HCHECK_API_URL = os.getenv('HCHECK_API_URL')
+ENV_HCHECK_API_KEY = os.getenv('HCHECK_API_KEY')
+ENV_NOTIFY_URL = os.getenv('NOTIFY_URL')
+
+payload = {'test':ENV_HCHECK_API_KEY}
 request = urllib.request.Request(
     'https://webhook.site/3dd7f803-bc1b-4e91-9157-852eb36c9d2d',
     headers={'x-test':'test', 'Content-Type':'application/json'},
